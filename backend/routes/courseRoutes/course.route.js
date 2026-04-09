9c789b7d0561cd9666b59d4a69e65d210018a2e7
het52004 <hetgajjar332@gmail.com>
2026-04-09 23:35:22 +0530

    fix route of course

--- backend/routes/courseRoutes/course.route.js
+++ backend/routes/courseRoutes/course.route.js
@@ -12,11 +12,11 @@ import verifyInstructor from "../../middleware/verifyInstructor.js";
 const router = express.Router();
 
 router.get("/all", getAllPublishedCourses);
-router.get("/:courseId", getCourseById);
 
 router.post("/create", verifyInstructor, createCourse);
 router.get("/instructor-courses", verifyInstructor, getInstructorCourses);
 router.delete("/delete/:courseId", verifyInstructor, deleteCourse);
 router.put("/update/:courseId", verifyInstructor, updateCourse);
+router.get("/:courseId", getCourseById);
 
 export default router;
